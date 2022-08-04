969d0aeff937866a9d774e21726c889c61bc0230
IbragimovaRoksana <roksalana91@mail.ru>
2022-08-04 12:01:59 +0500

    Added PaymentPage & CreditPage

--- src/main/java/ru/netology/page/StartPage.java
+++ src/main/java/ru/netology/page/StartPage.java
@@ -1,4 +1,18 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
 public class StartPage {
+    private SelenideElement heading = $("[h2.heading]");
+    private SelenideElement buttonPayment = $$(".button").find(exactText("Купить"));
+    private SelenideElement buttonCredit = $$(".button").find(exactText("Купить в кредит"));
+    public StartPage(){
+        heading.shouldBe(visible);
+    }
+
 }
